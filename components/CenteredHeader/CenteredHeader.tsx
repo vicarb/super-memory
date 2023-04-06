0bbed66c37e0821bb3936865dd9eee876a294267
vicarb <vcardenasb@protonmail.com>
2023-04-06 11:54:26 -0400

    centered header margin

--- components/CenteredHeader/CenteredHeader.tsx
+++ components/CenteredHeader/CenteredHeader.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const CenteredHeader = ({ header, subheader }: Props) => {
   return (
-    <div className="text-center">
+    <div className="text-center mt-4">
       <h1 className="text-3xl font-bold">{header}</h1>
       <p className="text-lg text-gray-700">{subheader}</p>
     </div>
