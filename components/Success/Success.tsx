4ff28131f24d52da109151e9b6808bd7416cb01d
vicarb <vcardenasb@protonmail.com>
2023-04-12 21:18:13 -0400

    success page created

---
+++ components/Success/Success.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Success = () => {
+  return (
+    <div className='mt-40'>
+      <h1 className="text-2xl">Payment Successful!</h1>
+      <p>Thank you for your purchase.</p>
+    </div>
+  );
+};
+
+export default Success;
