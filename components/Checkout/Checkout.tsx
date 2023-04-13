bf7c651f8b2a5478f400480d50c98326ef535a6c
vicarb <vcardenasb@protonmail.com>
2023-04-13 13:02:41 -0400

    checkout page update

--- components/Checkout/Checkout.tsx
+++ components/Checkout/Checkout.tsx
@@ -21,112 +21,102 @@ const Checkout = () => {
     console.log(formData)
   }
   return(
-    <div class="container mx-auto p-4 mt-16">
-  <div class="md:flex md:justify-center">
-    <div class="md:w-1/2 px-8 py-6">
-      <h2 class="font-bold text-lg mb-2">Delivery Information</h2>
-      <form class="grid grid-cols-2 gap-4">
-        <div class="col-span-2 sm:col-span-1">
-          <label for="name" class="block text-gray-700 font-bold mb-2">
-            Name
-          </label>
-          <input
-            id="name"
-            type="text"
-            class="form-input w-full"
-            placeholder="John Doe"
-          />
-        </div>
-        <div class="col-span-2 sm:col-span-1">
-          <label for="email" class="block text-gray-700 font-bold mb-2">
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            class="form-input w-full"
-            placeholder="johndoe@example.com"
-          />
-        </div>
-        <div class="col-span-2 sm:col-span-1">
-          <label for="address" class="block text-gray-700 font-bold mb-2">
-            Address
-          </label>
-          <input
-            id="address"
-            type="text"
-            class="form-input w-full"
-            placeholder="123 Main St"
-          />
-        </div>
-        <div class="col-span-2 sm:col-span-1">
-          <label for="city" class="block text-gray-700 font-bold mb-2">
-            City
-          </label>
-          <input
-            id="city"
-            type="text"
-            class="form-input w-full"
-            placeholder="San Francisco"
-          />
-        </div>
-        <div class="col-span-2 sm:col-span-1">
-          <label for="state" class="block text-gray-700 font-bold mb-2">
-            State
-          </label>
-          <input
-            id="state"
-            type="text"
-            class="form-input w-full"
-            placeholder="CA"
-          />
-        </div>
-        <div class="col-span-2 sm:col-span-1">
-          <label for="zip" class="block text-gray-700 font-bold mb-2">
-            ZIP
-          </label>
-          <input
-            id="zip"
-            type="text"
-            class="form-input w-full"
-            placeholder="94103"
-          />
-        </div>
-        <div class="col-span-2 mt-4">
-          <button
-            type="submit"
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Pay Now
-          </button>
-        </div>
-      </form>
-    </div>
-    <div class="md:w-1/2 px-8 py-6">
-      <div class="border rounded-md shadow-md p-4">
-        <h2 class="font-bold text-lg mb-2">Order Summary</h2>
-        <div class="flex justify-between border-t pt-2 mt-2">
-          <p class="text-gray-500">3 items</p>
-          <p class="text-gray-500">$50</p>
-        </div>
-        <div class="flex justify-between border-t pt-2 mt-2">
-          <p class="text-gray-200">Hello</p>
-          </div>
-        <div class="flex justify-between border-t pt-2 mt-2">
-          <p class="text-gray-500">Shipping</p>
-          <p class="text-gray-500">$5</p>
-        </div>
-        <div class="flex justify-between border-t pt-2 mt-2">
-          <p class="text-gray-500">Tax</p>
-          <p class="text-gray-500">$2.5</p>
-        </div>
-        <div class="flex justify-between border-t pt-2 mt-2">
-          <p class="font-bold">Total</p>
-          <p class="font-bold">$57.5</p>
+    <div className="container mx-auto mt-24">
+    <div className="flex flex-wrap -mx-4">
+      <div className="w-full md:w-1/2 px-4">
+        <div className="border rounded-md shadow-md p-4">
+          <h2 className="font-bold text-lg mb-2">Delivery Information</h2>
+          <form>
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="name"
+                type="text"
+                placeholder="John Doe"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="johndoe@example.com"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="address">
+                Address
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="address"
+                type="text"
+                placeholder="123 Main St"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="city">
+                City
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="city"
+                type="text"
+                placeholder="Anytown"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="state">
+                State
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="state"
+                type="text"
+                placeholder="CA"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2" htmlFor="zip">
+                Zip
+              </label>
+              <input
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="zip"
+                type="text"
+                placeholder="12345"
+              />
+            </div>
+          </form>
         </div>
       </div>
+      <div className="w-full md:w-1/2 px-8">
+  <div className="border rounded-md shadow-md p-4">
+    <h2 className="font-bold text-lg mb-2">Order Summary</h2>
+    <div className="flex justify-between border-t pt-2 mt-2">
+      <p className="text-gray-500">3 items</p>
+      <p className="text-gray-500">$50</p>
     </div>
+    <div className="flex justify-between border-t pt-2 mt-2">
+      <p className="text-gray-500">Delivery</p>
+      <p className="text-gray-500">$5</p>
+    </div>
+    <div className="flex justify-between border-t pt-2 mt-2">
+      <p className="text-gray-500 font-bold">Total</p>
+      <p className="text-gray-500 font-bold">$55</p>
+    </div>
+    <button className="bg-blue-500 text-white font-bold py-2 px-4 mt-4 rounded-md hover:bg-blue-600">
+      Pay Now
+    </button>
   </div>
+</div>
+</div>
 </div>
 
 
